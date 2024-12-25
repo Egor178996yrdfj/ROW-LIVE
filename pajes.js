11ddb9cb2df755a4d42a7313ef6fde9877f9396f
Egor178996yrdfj <lepkoegor01@gmail.com>
2024-12-25 19:18:34 +0300

    Update pajes.js

--- pajes.js
+++ pajes.js
@@ -1,1 +1,10 @@
+document.ondragstart = noselect;
+document.onselectstart = noselect;
+function noselect() {return false;}
 
+let tg = window.Telegram.WebApp;
+let champ = document.getElementById('champrang');
+
+champ.getElementById('click', () => {
+    tg.close();
+});
