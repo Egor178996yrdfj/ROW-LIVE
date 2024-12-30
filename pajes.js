52ca972cb5d4c8ae3acee35dcbe1621dc1ec4b0a
Egor178996yrdfj <lepkoegor01@gmail.com>
2024-12-30 16:22:44 +0300

    Update pajes.js

--- pajes.js
+++ pajes.js
@@ -18,6 +18,26 @@ buttonbackage_btn.addEventListener('click', () => {
     document.getElementById('account_age').style.display = 'none';
 })
 
+
+var rowscore = 0;
+
+var score = document.getElementById('rowscore');
+
+
+//daily reward
+
+var DayNumb0 = 400;
+var DayNumb1 = 520;
+var DayNumb2 = 640;
+var DayNumb3 = 760;
+var DayNumb4 = 880;
+var DayNumb5andBiger = 1100;
+
+var day_number = document.getElementById('day_number');
+var rowdayly_text_id = document.getElementById('rowdayly_text_id');
+
+//receiving an award
+
 let daily_claim_btn = document.getElementById('daily_claim_btn_img');
 let id_dark_back = document.getElementById('id_dark_back');
 
@@ -29,14 +49,41 @@ id_dark_back.addEventListener('click', () => {
     document.getElementById('daily_reward').style.display = 'none';
 })
 
+var day_numberNumb = 0;
+day_number.innerHTML = day_numberNumb;
 
-var id = setInterval(score, 1000);
+rowdayly_text_id.innerHTML = ("+" + DayNumb0 + "ROW");
+score.innerHTML = '0';
 
-var rowscore = 1075;
+const dayly_reward_con = () => {
+    day_numberNumb ++ ;
+    day_number.innerHTML = day_numberNumb;
+    if (day_numberNumb == 0) {
+        rowdayly_text_id.innerHTML = ("+" + DayNumb0 + "ROW");
+        score.innerHTML = (rowscore + DayNumb0);
+    } else if (day_numberNumb == 1) {
+        rowdayly_text_id.innerHTML = ("+" + DayNumb1 + "ROW");
+        score.innerHTML = (rowscore += DayNumb0);
+    } else if (day_numberNumb == 2) {
+        rowdayly_text_id.innerHTML = ("+" + DayNumb2 + "ROW");
+        score.innerHTML = (rowscore += DayNumb1);
+    } else if (day_numberNumb == 3) {
+        rowdayly_text_id.innerHTML = ("+" + DayNumb3 + "ROW");
+        score.innerHTML = (rowscore += DayNumb2);
+    } else if (day_numberNumb == 4) {
+        rowdayly_text_id.innerHTML = ("+" + DayNumb4 + "ROW");
+        score.innerHTML = (rowscore += DayNumb3);
+    } else if (day_numberNumb == 5) {
+        rowdayly_text_id.innerHTML = ("+" + DayNumb5andBiger + "ROW");
+        score.innerHTML = (rowscore += DayNumb4);
+    } else if (day_numberNumb >= 6) {
+        rowdayly_text_id.innerHTML = ("+" + DayNumb5andBiger + "ROW");
+        score.innerHTML = (rowscore += DayNumb5andBiger);
+    }
+};
 
-var score = document.getElementById('rowscore');
+daily_claim_btn.addEventListener('click', dayly_reward_con);
 
 setInterval(function() {
-    rowscore ++;
-    score.innerHTML=(rowscore);
-}, 1000);
+    document.getElementById('daily_reward').style.display = 'block';
+}, 10000);
