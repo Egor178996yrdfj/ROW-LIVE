6c1cacd5192b7019bdeb12f028a66652846bbce8
Egor178996yrdfj <lepkoegor01@gmail.com>
2025-01-17 15:46:30 +0300

    Update pajes.js

--- pajes.js
+++ pajes.js
@@ -211,4 +211,4 @@ if (tg && tg.initDataUnsafe && tg.initDataUnsafe.user) {
 usernameonAgeRewardID.innerHTML = readyResultRewardAge;
 
 //hat web app
-tg.setHeaderColor("#464646");
+tg.setHeaderColor("#131313");
